3d42c9a8b04ac6ccc6816864b4b2435767e69573
zouyonghao <yonghaoz1994@gmail.com>
2020-03-25 01:02:26 +0800

    ADD static graph

--- MyFunctionPass/MyFunctionPass.cpp
+++ MyFunctionPass/MyFunctionPass.cpp
@@ -1,35 +1,78 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Support/raw_ostream.h"
 
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
+#include "Graph.hpp"
+
 using namespace llvm;
+using namespace std;
 
 namespace {
-    class MyFunctionPass : public FunctionPass {
-    public:
-        static char ID;
+class MyFunctionPass : public FunctionPass {
+public:
+  static char ID;
 
-        MyFunctionPass() : FunctionPass(ID) {}
+  vector<Node *> nodes;
 
-        bool runOnFunction(Function &F) override {
-            outs() << "Hello: " << F.getName() << "\n";
-            return true;
-        }
-    };
-}
+  Node *main = nullptr;
+
+  MyFunctionPass() : FunctionPass(ID) {}
+
+  bool runOnFunction(Function &F) override;
+
+  bool doFinalization(Module &module) override;
+};
+} // namespace
 
 char MyFunctionPass::ID = 0;
+
+bool MyFunctionPass::runOnFunction(Function &F) {
+  outs() << "Hello: " << F.getName() << "\n";
+  Node *parentNode = Node::createOrGetFromVector(nodes, F.getName());
+  if (F.getName() == "main") {
+    main = parentNode;
+  }
+
+  if (parentNode->isScanned()) {
+    return false;
+  }
+
+  for (Function::iterator iter = F.begin(); iter != F.end(); ++iter) {
+    for (BasicBlock::iterator bb = iter->begin(); bb != iter->end(); ++bb) {
+      Instruction *I = &*bb;
+      if (auto *inst = dyn_cast_or_null<CallInst>(I)) {
+        Function *called = inst->getCalledFunction();
+        if (called) {
+          Node *childNode =
+              Node::createOrGetFromVector(nodes, called->getName());
+          parentNode->addEdge(childNode);
+        }
+      }
+    }
+  }
+
+  parentNode->setScan(true);
+
+  return true;
+}
+
+bool MyFunctionPass::doFinalization(Module &module) {
+  main->print(0);
+  return Pass::doFinalization(module);
+}
 
 static RegisterPass<MyFunctionPass> X("myfuncpass", "My Function Pass");
 
 static void registerPass(const PassManagerBuilder &,
                          legacy::PassManagerBase &PM) {
-    PM.add(new MyFunctionPass());
+  PM.add(new MyFunctionPass());
 }
-
 static RegisterStandardPasses
-        RegisterTheSpindlePass(PassManagerBuilder::EP_EarlyAsPossible, registerPass);
+    RegisterTheSpindlePass(PassManagerBuilder::EP_EarlyAsPossible,
+                           registerPass);
---
+++ test.sh
@@ -0,0 +1,1 @@
+clang -Xclang -load -Xclang ./cmake-build-debug/MyFunctionPass/libMyFunctionPass.so test.c
